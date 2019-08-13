c03e72ab55ebdd92d25e433adff2cb21c38e34ee
Xingyu <nick19960723@gmail.com>
2019-08-13 23:27:11 +0800

    test

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/public',express.static('practice_yiyi_image'));
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
